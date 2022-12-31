e29078b19a22f83a0ac67b33a6a943fed25c1b7e
Ryan DeLong <rdelong111@gmail.com>
2022-12-31 16:54:03 -0600

    commit tree 4e8b4e58508d0717a1d1752cfb9506b6668eca82

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(string)
+  stack = Stack.new
+  string.chars.each do |s|
+    if s == "("
+      stack.push(s)
+    else
+      if stack.peek == "("
+        stack.pop
+      else
+        stack.push(s)
+      end
+    end
+  end
+  stack.size
+end
